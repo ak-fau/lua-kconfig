57b6a2d9a0ba44601bfb9caba49cdb2fd1af976e
Anton Kuzmin <anton.kuzmin@cs.fau.de>
2020-03-16 14:40:27 +0100

    Export table to the Kconfig formatted string -- tests for hierarchy and prefix support

--- spec/tostring_spec.lua
+++ spec/tostring_spec.lua
@@ -2,11 +2,9 @@ require "pl" -- penlight library (for stringio)
 
 local kconfig = require "kconfig"
 local kload = kconfig.load
+local hierarchy = kconfig.hierarchy
 
-local function test_x(s)
-  local t = kload(stringio.open(s))
-  assert.is_equal(s, tostring(t))
-end
+local test_x
 
 local function test_string()
   test_x [[LUA_STRING="Lua test string with quotes: \", '"
@@ -49,6 +47,69 @@ LUA_STRING="Lua test string with quotes: \", '"
 end
 
 describe("#tostring", function()
+
+           setup(function()
+               test_x = function(s)
+                 local t = kload(stringio.open(s))
+                 assert.is_equal(s, tostring(t))
+               end
+           end)
+
+           it("#string", test_string)
+           it("#boolean #true", test_bool_true)
+           it("#boolean #false", test_bool_false)
+           it("#int", test_integer)
+           it("#hex", test_hex)
+           it("#full config with all types", test_full)
+end)
+
+describe("#tostring #hierarchy", function()
+
+           setup(function()
+               test_x = function(s)
+                 local t = kload(stringio.open(s))
+                 t = hierarchy(t)
+                 assert.is_equal(s, tostring(t))
+               end
+           end)
+
+           it("#string", test_string)
+           it("#boolean #true", test_bool_true)
+           it("#boolean #false", test_bool_false)
+           it("#int", test_integer)
+           it("#hex", test_hex)
+           it("#full config with all types", test_full)
+end)
+
+describe("#tostring #prefix", function()
+           local prefix = "lua"
+
+           setup(function()
+               test_x = function(s)
+                 local t = kload(stringio.open(s), prefix)
+                 assert.is_equal(s, tostring(t))
+               end
+           end)
+
+           it("#string #debug", test_string)
+           it("#boolean #true", test_bool_true)
+           it("#boolean #false", test_bool_false)
+           it("#int", test_integer)
+           it("#hex", test_hex)
+           it("#full config with all types", test_full)
+end)
+
+describe("#tostring #hierarchy #prefix", function()
+           local prefix = "lua"
+
+           setup(function()
+               test_x = function(s)
+                 local t = kload(stringio.open(s), prefix)
+                 t = hierarchy(t)
+                 assert.is_equal(s, tostring(t))
+               end
+           end)
+
            it("#string", test_string)
            it("#boolean #true", test_bool_true)
            it("#boolean #false", test_bool_false)
